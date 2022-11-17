5eb7b0be5829af0fdf19fe64c560d8712bf94139
Dinara83 <108099459+Dinara83@users.noreply.github.com>
2022-11-17 18:30:27 +0200

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,53 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const refs = {
+  galleryEl: document.querySelector('.gallery'),
+  body: document.querySelector('body'),
+};
+
+const makeGalleryItemsColection = ({ preview, original, description }) => {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+	<img 
+	  class="gallery__image lazyload"
+	  data-src="${preview}"
+	  data-source="${original}"
+	  alt="${description}"
+	/>
+  </a>
+  </div>`;
+};
+
+const makeColectionImage = galleryItems.map(makeGalleryItemsColection).join('');
+
+refs.galleryEl.insertAdjacentHTML('beforeend', makeColectionImage);
+refs.body.addEventListener('click', onOpenGalleryColection);
+
+function onOpenGalleryColection(evt) {
+  evt.preventDefault();
+  const isGalleryImage = evt.target.classList.contains('gallery__image');
+  if (!isGalleryImage) {
+    return;
+  }
+  window.instance = basicLightbox.create(
+    `
+		<img src="${evt.target.dataset.source}">
+		`
+  );
+  instance.show();
+  refs.galleryEl.addEventListener('keydown', evt => {
+    if (evt.key === 'Escape') {
+      instance.close();
+    }
+  });
+}
+
+const lazyImages = document.querySelectorAll('.lazyload');
+lazyImages.forEach(image => {
+  image.addEventListener('load', onImageLoaded, { once: true });
+});
+
+function onImageLoaded(evt) {
+  evt.target.classList.add('appear');
+}
