6cc314217ab994aa543f060e31b9551d821d6070
Dinara83 <108099459+Dinara83@users.noreply.github.com>
2022-11-18 16:57:21 +0200

    video

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,49 +1,28 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
-
+import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 const refs = {
   galleryEl: document.querySelector('.gallery'),
   body: document.querySelector('body'),
 };
 
 const makeGalleryItemsColection = ({ preview, original, description }) => {
-  return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-	<img 
-	  class="gallery__image lazyload"
-	  data-src="${preview}"
-	  data-source="${original}"
-	  alt="${description}"
-	/>
-  </a>
-  </div>`;
+  return `<a class="gallery__item" href="${original}">
+	<img class="gallery__image lazyload" src="${preview}" alt="${description}" />
+  </a>`;
 };
-
 const makeColectionImage = galleryItems.map(makeGalleryItemsColection).join('');
 
 refs.galleryEl.insertAdjacentHTML('beforeend', makeColectionImage);
-refs.body.addEventListener('click', onOpenGalleryColection);
 
-function onOpenGalleryColection(evt) {
-  evt.preventDefault();
-  const isGalleryImage = evt.target.classList.contains('gallery__image');
-  if (!isGalleryImage) {
-    return;
-  }
-  window.instance = basicLightbox.create(
-    `
-		<img src="${evt.target.dataset.source}">
-		`
-  );
-  instance.show();
-  refs.galleryEl.addEventListener('keydown', evt => {
-    if (evt.key === 'Escape') {
-      instance.close();
-    }
-  });
-}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  scrollZoom: false,
+});
 
 const lazyImages = document.querySelectorAll('.lazyload');
 lazyImages.forEach(image => {
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const vimeoPlayer = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(vimeoPlayer);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
